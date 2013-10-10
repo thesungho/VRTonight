81b4699da5a5534b4eab84f7fe12cfb0e2b9e4ef
Sungho Yoo <sunghoster@gmail.com>
2013-10-10 16:11:25 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VRTonight::Application.config.secret_token = 'dbc7f2ddfbd6e30e939e2a70b81bcc8d75b6eb8b1994a181dad0a52490c575db491de313a6ba3b48e95081c99492e319000e1c5076472f94ac1b7c7384049865'
